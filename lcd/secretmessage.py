b8bb9249cf60a68a0218486d70d1d49f564aba17
Jeff Arends <jeffarends@gmail.com>
2022-06-22 14:56:02 -0600

    added a message for ctfd

---
+++ lcd/secretmessage.py
@@ -0,0 +1,13 @@
+import lcd_i2c
+from time import sleep
+from base64 import b64decode
+
+# Padding the strings
+
+list = [1, -19, 27, 22, -15, 2, 13, 14, 33, 19, 6, 21, 23, 16, 75, 5]
+
+lcd_i2c.lcd_init()
+
+for i, val in enumerate(list):
+    #lcd_i2c.lcd_byte(i, 0)
+    lcd_i2c.lcd_byte(100 - val + 2 * i, 1)
