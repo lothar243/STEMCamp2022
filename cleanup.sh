30ec242f7eb434e13dde0b6304d2dfc3d2f1bbe4
jeff <jeffarends@gmail.com>
2022-07-11 20:53:17 -0600

    Added a cleanup script to fix unzipping everything in the home directory

---
+++ cleanup.sh
@@ -0,0 +1,21 @@
+rm -r ~/.pacman
+rm -r ~/ciphers
+rm -r ~/dotmatrix
+rm -r ~/grep_game
+rm -r ~/hashes
+rm -r ~/lcd
+rm -r ~/morseCode
+rm -r ~/passwords
+rm -r ~/rfid
+rm -r ~/scavenger-hunt
+rm -r ~/serialChip
+rm -r ~/wireshark
+rm ~/Handout.docx
+rm ~/afterdownload.sh
+rm ~/c_law.dd
+rm ~/ciphor.py
+rm ~/compiledBinary
+rm ~/convertToUTF8.sh
+rm ~/dotMatrix-onepixel.jpg
+rm ~/ghost_hunt_score.py
+rm ~/rsaHex.py
