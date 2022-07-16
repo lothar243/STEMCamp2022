83dd78fe92ce5e48ae33243f0948f5d9d351654d
lothar243 <jeffarends@gmail.com>
2022-07-16 07:48:37 -0600

    Moving the ghost hunt to the team server

--- afterdownload.sh
+++ afterdownload.sh
@@ -1,12 +1,3 @@
-# Deploying pacman ghosts
-cp .pacman/g1 /home/pi/Desktop/8linky.txt
-cp .pacman/g2 /home/pi/Desktop/PiNkY.txt
-cp .pacman/g3 /home/pi/Templates/1nky.txt
-cp .pacman/g4 /home/pi/Downloads/CLYDE.txt
-cp .pacman/g5 /home/pi/Documents/sp00ky.txt
-cp .pacman/g6 /home/pi/Documents/funky.txt
-cp .pacman/g7 /home/pi/Music/sue.txt
-
 echo "Setting up ciphor.py to be executed from anywhere"
 sudo cp ciphor.py /usr/bin
 sudo chmod +x /usr/bin/ciphor.py
